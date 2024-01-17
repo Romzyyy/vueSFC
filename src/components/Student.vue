cc6c894d2ce58c247097ae45cd35cbd35202fce3
romzyyy <fijay0117@gmail.com>
2024-01-17 23:40:47 +0700

    study: emits, props, component and slot

---
+++ src/components/Student.vue
@@ -0,0 +1,19 @@
+<template>
+    <slot>this is default solout</slot>
+    <p>hello, this is from student component my name is {{ studentName }} </p>
+    <button @click="showData">click me</button>
+</template>
+
+<script>
+export default {
+    props: {
+        studentName : String
+    },
+    emits : ['response'],
+    methods: {
+        showData(){
+            this.$emit('response', this.studentName)
+        }
+    }
+}
+</script>
